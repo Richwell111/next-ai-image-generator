a75c7db4aeee343521cbf3da02b1495a09f1def8
Richwell111 <bcict20103@gmail.com>
2026-02-15 14:17:30 +0000

    feat: Initialize Next.js project with Prisma, authentication, dashboard structure, and global styling.

---
+++ frontend/src/lib/prisma.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
+
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+ 
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
